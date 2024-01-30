b04b110604f8a4ea4ddbb42d8313a8f17a181e76
First test <volchenkov.andryuha@yandex.ru>
2024-01-30 11:28:54 +0300

    main

---
+++ untitled/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 5_476;
+        int ruble = 20;
+        int bonus;
+
+        bonus = ticket / ruble;
+
+        System.out.println("Бонус за купленный билет:" + bonus);
+
+
+    }
+}
